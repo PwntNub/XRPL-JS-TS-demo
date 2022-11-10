d3ff014219ced30af3acb76081e79fcd94dcca42
pwnt.nub@hotmail.com <pwnt.nub@hotmail.com>
2022-11-10 16:50:12 -0600

    SignTransactions

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
-if (process.argv.length < 3) {
-  console.log("Usage: node dist/index <r-address>");
+if (process.argv.length < 6) {
+  console.log(
+    "\n\nUsage: node dist/index <secret_type> <secret> <destination_address> <amount>\n\n"
+  );
+  console.log("secret type\n1, Mnemonic\n2, familySeed\n\n");
   process.exit(1);
 }
 
@@ -15,33 +18,68 @@ import { derive, sign, utils } from "xrpl-accountlib";
 //    > https://www.npmjs.com/package/xumm-sdk
 //    > https://dev.to/wietse/how-to-use-the-xumm-sdk-in-node-js-5380
 
-//const secret = "sEdTriaMMLbaWru9eZRbrQhPdMP4Q3v";
-// account = rsDT47FbAU8dwifYoDGrFTEUp6RYta5ZJ4
-const client = new XrplClient();
+const client = new XrplClient("wss://s.altnet.rippletest.net:51233");
 
 const main = async () => {
-  if (!utils.isValidAddress(process.argv[2])) {
-    console.log("Account not found or is invalid!");
-    process.exit(1);
+  let account: any;
+  if (process.argv[2] === "1") {
+    if (!utils.isValidMnemnic(process.argv[3])) {
+      console.log("Mnemonic is invalid!");
+      process.exit(1);
+    }
+    account = derive.mnemonic(process.argv[3]);
   }
-
+  if (process.argv[2] === "2") {
+    if (!utils.isValidSeed(process.argv[3])) {
+      console.log("Seed is invalid!");
+      process.exit(1);
+    }
+    account = derive.familySeed(process.argv[3]);
+  }
   const data = await client.send({
     command: "account_info",
-    account: process.argv[2],
+    account: account.address,
     strict: true,
   });
-
+  console.log("Data: ", data);
   if (data.error) {
     console.log("Error: ", data.error_message);
     process.exit(1);
   }
 
-  console.log("Data ", data);
+  if (Number(data.account_data.Balance) / 1000000 < Number(process.argv[5])) {
+    console.log("Warning: Your balance is low");
+    console.log("You must have 10 XRP + the amount you are sending\n");
+    process.exit(1);
+  }
+  const { id, signedTransaction } = sign(
+    {
+      TransactionType: "Payment",
+      Account: account.address,
+      Destination: process.argv[4],
+      Amount: String(1_000_000),
+      Sequence: data.account_data.Sequence,
+      Fee: String(12),
+    },
+    account
+  );
+  console.log("id:  ", id);
+  console.log("Signed Transaction:  ", signedTransaction);
+
+  /*
+  if (data.error) {
+    console.log("Error: ", data.error_message);
+    process.exit(1);
+  }
+  
   console.log("LedgerEntryType: ", data.account_data.LedgerEntryType);
   console.log("Balance: ", data.account_data.Balance / 1000000, " XRP");
   console.log("OwnerCount: ", data.account_data.OwnerCount);
   console.log("Sequence: ", data.account_data.Sequence);
   console.log("Current Ledger Index: ", data.ledger_current_index);
+  */
+
+  process.exit(1);
 };
 
 main();
